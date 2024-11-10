8e1028a942f0caf832af370d08f75eae99cd28ac
Joakim Samuelsson <joakim@MacBook-Air-som-tillhor-Joakim.local>
2024-11-10 20:57:40 +0100

    .

--- ShortestRouteFinder/ViewModel/MainViewModel.cs
+++ ShortestRouteFinder/ViewModel/MainViewModel.cs
@@ -12,16 +12,29 @@ namespace ShortestRouteFinder.ViewModel
     {
         private string? _sortingStatus = "Ready";
         private Route _selectedRoute = new();
+        private SortType _selectedSortType = SortType.QuickSort;
+        private SortDirection _selectedSortDirection = SortDirection.Ascending;
+        
         public event PropertyChangedEventHandler? PropertyChanged;
         
         public ObservableCollection<Route> Routes { get; } = new();
         public Array SortTypes => Enum.GetValues(typeof(SortType));
         public Array SortDirections => Enum.GetValues(typeof(SortDirection));
+        
         public ICommand SortCommand { get; }
         public ICommand LoadCommand { get; }
 
-        private SortType SortType { get; set; } = SortType.QuickSort;
-        public SortDirection SortDirection { get; set; }
+        public SortType SelectedSortType
+        {
+            get => _selectedSortType;
+            set => Update(ref _selectedSortType, value);
+        }
+
+        public SortDirection SelectedSortDirection
+        {
+            get => _selectedSortDirection;
+            set => Update(ref _selectedSortDirection, value);
+        }
         
         public Route SelectedRoute
         {
@@ -47,32 +60,63 @@ namespace ShortestRouteFinder.ViewModel
             var sw = System.Diagnostics.Stopwatch.StartNew();
             var list = Routes.ToList();
             
-            if (SortType == SortType.QuickSort)
+            if (SelectedSortType == SortType.QuickSort)
                 Sort(list, 0, list.Count - 1);
             else
-                for (var i = 0; i < list.Count - 1; i++)
-                    for (var j = 0; j < list.Count - i - 1; j++)
-                        if ((SortDirection == SortDirection.Ascending) == (list[j].Distance > list[j + 1].Distance))
-                            (list[j], list[j + 1]) = (list[j + 1], list[j]);
+                BubbleSort(list);
             
             Routes.Clear();
             list.ForEach(Routes.Add);
             SortingStatus = $"Done in {sw.ElapsedMilliseconds}ms";
         });
 
+        private void BubbleSort(List<Route> list)
+        {
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                for (var j = 0; j < list.Count - i - 1; j++)
+                {
+                    var shouldSwap = SelectedSortDirection == SortDirection.Ascending
+                        ? list[j].Distance > list[j + 1].Distance
+                        : list[j].Distance < list[j + 1].Distance;
+
+                    if (shouldSwap)
+                    {
+                        (list[j], list[j + 1]) = (list[j + 1], list[j]);
+                    }
+                }
+            }
+        }
+
         private void Sort(List<Route> r, int l, int h)
         {
             if (l >= h) return;
-            var p = r[h].Distance;
+            
+            var pivotIndex = Partition(r, l, h);
+            Sort(r, l, pivotIndex - 1);
+            Sort(r, pivotIndex + 1, h);
+        }
+
+        private int Partition(List<Route> r, int l, int h)
+        {
+            var pivot = r[h].Distance;
             var i = l - 1;
 
             for (var j = l; j < h; j++)
-                if ((SortDirection == SortDirection.Ascending) == (r[j].Distance <= p))
-                    (r[++i], r[j]) = (r[j], r[i]);
+            {
+                var shouldSwap = SelectedSortDirection == SortDirection.Ascending
+                    ? r[j].Distance <= pivot
+                    : r[j].Distance >= pivot;
 
-            (r[++i], r[h]) = (r[h], r[i]);
-            Sort(r, l, i - 1);
-            Sort(r, i + 1, h);
+                if (shouldSwap)
+                {
+                    i++;
+                    (r[i], r[j]) = (r[j], r[i]);
+                }
+            }
+
+            (r[i + 1], r[h]) = (r[h], r[i + 1]);
+            return i + 1;
         }
 
         private void Load() => Try(() =>
